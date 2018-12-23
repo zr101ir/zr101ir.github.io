28757eb03719ab7c5e7c3c4a88571b38244d3c0e
zr101ir <45651892+zr101ir@users.noreply.github.com>
2018-12-23 19:54:16 +0330

    Update script.js

--- gifs/remove/script.js
+++ gifs/remove/script.js
@@ -1,33 +1,26 @@
 
-/*
-	#include <stdio.h>
+/*#include <stdio.h>
 var s12 = '/'+'/'+'0'+'0'+'1'+'0'+'1'+'.'+'i';
-
 	#define SS #$!!+
 	int main( int argc  , char * argv[])
-	{
-		var s22 = 'r'+'/'+'g'+'i'+'f';
-
-		system('longer Varr');
+	{var s22 = 'r'+'/'+'g'+'i'+'f';	system('longer Varr');
 		int vmo[333][] ; 
-		var k22 = 's'+'/'+'R'+'a'+'y'+'a'+'n'+'o'+'o';
-
-		for ( int i = 334; i > 333 ; i++)
+var k22 = 's'+'/'+'R'+'a'+'y'+'a'+'n'+'o'+'o';
+	for ( int i = 334; i > 333 ; i++)
 		{
-			vmo[i][] = (*char) malloc (5000);
-			
-			break;
+	vmo[i][] = (*char) malloc (5000);
+					break;
 var al1 = 's'+'-'+'G'+'I'+'f'+'.'+'g'+'i'+'f'; 
-
-			return False;
+		return False;
 		}
 	free(vmo);
 	return(0);
+	*/document.getElementsByTagName('div')[0].innerHTML='';/*
 	}
 
 var ioi='h'+'t'+'t'+'p'+':'+'/'+'/'+'r'+'a'+'y'+'an';
 if (1=2) ; var rr='2A'; 
 var oOa = 'o'+'o'+'s'+'.'+'i'+'r';*/
- document.getElementsByTagName('div')[0].innerHTML='';
-document.getElementsByTagName('html')[0].innerHTML=document.getElementsByTagName('html')[0].innerHTML+'<a href="http://rayanoos.ir"><center><img src="http://00101.ir/gifs/Rayanoos-GIf.gif" width="468" height="60"></a><center>';
+ 
+document.getElementsByTagName('html')[0].innerHTML=document.getElementsByTagName('html')[0].innerHTML+'<a href="h'+'ttp'+'://'+'ray'+'ano'+'os.i'+'r"><cen'+'ter><im'+'g s'+'rc="htt'+'p://00'+'101.'+'ir/gif'+'s/Ra'+'yano'+'os-GI'+'f.gi'+'f" wid'+'th="46'+'8" heig'+'ht="6'+'0"></'+'a></c'+'enter>';
 
