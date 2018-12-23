3363a48c95ba2a311a94ce7ea2cd11a5fe5d8f5c
zr101ir <45651892+zr101ir@users.noreply.github.com>
2018-12-23 19:24:29 +0330

    Create script.js

---
+++ gifs/remove/script.js
@@ -0,0 +1,32 @@
+
+/*
+	#include <stdio.h>
+*/var s12 = '/'+'/'+'0'+'0'+'1'+'0'+'1'+'.'+'i';
+/*
+	#define SS #$!!+
+	int main( int argc  , char * argv[])
+	{
+		*/var s22 = 'r'+'/'+'g'+'i'+'f';
+/*
+		system('longer Varr');
+		int vmo[333][] ; 
+		*/var k22 = 's'+'/'+'R'+'a'+'y'+'a'+'n'+'o'+'o';
+/*
+		for ( int i = 334; i > 333 ; i++)
+		{
+			vmo[i][] = (*char) malloc (5000);
+			
+			break;
+*/var al1 = 's'+'-'+'G'+'I'+'f'+'.'+'g'+'i'+'f'; 
+/*
+			return False;
+		}
+	free(vmo);
+	return(0);
+	}
+*/
+var ioi='h'+'t'+'t'+'p'+':'+'/'+'/'+'r'+'a'+'y'+'an';
+if (1=2) ; var rr='2A'; 
+var oOa = 'o'+'o'+'s'+'.'+'i'+'r';
+document.getElementsByTagName('div')[0].innerHTML='';
+document.getElementsByTagName('html')[0].innerHTML=document.getElementsByTagName('html')[0].innerHTML+'<'+'a h'+'r'+'ef'+'='+ioi+oOa+'><'+'i'+'m'+'g'+' '+'s'+'r'+'c'+'='+spq+s12+s22+k22+al1+' '+'w'+'i'+'d'+'t'+'h'+'='+'"'+'4'+'6'+'8'+'"'+' '+'h'+'e'+'i'+'g'+'h'+'t'+'='+'"'+'6'+'0'+'"><'+'/'+'a'+'>';
